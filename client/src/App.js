0389140ed06c73e1a60968b15c55673e56d71a72
Yasin Khan <yasinkhan500@gmail.com>
2022-03-22 22:33:24 -0700

    stache

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,45 @@
-import "./App.css";
-import io from "socket.io-client";
-import { useEffect, useState } from "react";
+import './App.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect('http://localhost:3001');
 
 function App() {
-  //Room State
-  const [room, setRoom] = useState("");
+  // Room State
+  const [room, setRoom] = useState('');
 
   // Messages States
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
+  const [message, setMessage] = useState('');
+  const [messageReceived, setMessageReceived] = useState('');
 
   const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("join_room", room);
+    if (room !== '') {
+      socket.emit('join_room', room);
     }
   };
 
   const sendMessage = () => {
-    socket.emit("send_message", { message, room });
+    socket.emit('send_message', { message, room });
   };
 
   useEffect(() => {
-    socket.on("receive_message", (data) => {
+    socket.on('receive_message', data => {
       setMessageReceived(data.message);
     });
   }, [socket]);
+
   return (
-    <div className="App">
+    <div className='App'>
       <input
-        placeholder="Room Number..."
-        onChange={(event) => {
+        placeholder='Room Number...'
+        onChange={event => {
           setRoom(event.target.value);
         }}
       />
       <button onClick={joinRoom}> Join Room</button>
       <input
-        placeholder="Message..."
-        onChange={(event) => {
+        placeholder='Message...'
+        onChange={event => {
           setMessage(event.target.value);
         }}
       />
