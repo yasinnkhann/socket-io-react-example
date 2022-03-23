86cd929addb8f97f871d8425989b58f1a9470a53
Yasin Khan <yasinkhan500@gmail.com>
2022-03-22 22:10:18 -0700

    stache

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
 app.use(cors());
 
@@ -10,23 +10,23 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
   },
 });
 
-io.on("connection", (socket) => {
+io.on('connection', socket => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
+  socket.on('join_room', data => {
     socket.join(data);
   });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+  socket.on('send_message', data => {
+    socket.to(data.room).emit('receive_message', data);
   });
 });
 
 server.listen(3001, () => {
-  console.log("SERVER IS RUNNING");
+  console.log('SERVER IS RUNNING');
 });
